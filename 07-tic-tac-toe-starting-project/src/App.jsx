fcc3f8a3850e4a3a248734b8bd843a2d53fee4ac
karun sharma <sharmakarun06@gmail.com>
2023-11-23 22:43:08 +0530

    reducing state

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,20 +3,22 @@ import Player from "./components/Player";
 import Log from "./components/Log";
 import { useState } from "react";
 
+function deriveActivePlayer(gameTurns){
+  let activePlayer = 'X';
+  if( gameTurns.length > 0 &&  gameTurns[0].player === 'X'){
+    activePlayer = 'O';
+  }
+  return activePlayer; 
+}
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
   
-  function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X'?'O':'X');
-    setGameTurns((prevTurns) => { 
-      let currentPlayer = 'X';
-      
-      if( prevTurns.length > 0 && prevTurns[0].player === 'X'){
-        currentPlayer = 'O';
-      } 
+  const activePlayer =  deriveActivePlayer(gameTurns);
 
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameTurns((prevTurns) => { 
+      const currentPlayer = deriveActivePlayer(prevTurns);
       const updatedTurns = [
         { square: {row: rowIndex, col: colIndex}, player: currentPlayer}, 
         ...prevTurns,
