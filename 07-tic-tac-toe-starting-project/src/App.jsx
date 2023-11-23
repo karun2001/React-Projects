74d484daa18e932a1fee98a90f87995cf374d25f
karun sharma <sharmakarun06@gmail.com>
2023-11-23 23:27:36 +0530

    added winner identification

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -2,6 +2,8 @@ import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
 import { useState } from "react";
+import { WINNING_COMBINATIONS } from "./winning-combination";
+
 
 function deriveActivePlayer(gameTurns){
   let activePlayer = 'X';
@@ -11,10 +13,38 @@ function deriveActivePlayer(gameTurns){
   return activePlayer; 
 }
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  
   const activePlayer =  deriveActivePlayer(gameTurns);
+
+  let winner = null;      
+
+  let gameBoard = initialGameBoard;
+  for(const turn of gameTurns) {
+      const { square, player} = turn;
+      const { col, row } = square;
+      gameBoard[row][col] = player;
+  }
+
+  for(const combination of WINNING_COMBINATIONS){
+    const firstSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if(
+      firstSymbol && 
+      firstSymbol == secondSymbol && 
+      firstSymbol == thirdSymbol){
+        winner = <li>{firstSymbol} is the Winner...!!</li>
+    }
+  }
+  
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) => { 
@@ -42,9 +72,10 @@ function App() {
           isActive={activePlayer === "O"}
         />
       </ol>
+      {winner}
       <GameBoard 
         onSelectSquare={handleSelectSquare} 
-        turns={gameTurns} 
+        board={gameBoard} 
       />
     </div>
     <Log turns = {gameTurns} />  
