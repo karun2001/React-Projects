4fe48578c679e65d51a30078fd10e02452d2273c
karun sharma <sharmakarun06@gmail.com>
2023-12-04 22:11:40 +0530

    adding styles using tailwind css

---
+++ section6-styling/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/logo.png';
+
+export default function Header() {
+  return (
+    <header className="flex flex-col items-center mt-8 mb-16">
+      <img src={logo} alt="A canvas" className="mb-8 w-44 h-44 object-contain" />
+      <h1 className="text-4xl font-title font-semibold tracking-widest text-center uppercase text-amber-800">ReactArt</h1>
+      <p className='text-stone-500'>A community of artists and art-lovers.</p>
+    </header>
+  );
+}
